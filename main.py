6c94efd1f1761486914ae41f9b028ecd9a99899d
Luis G D Simão <luisgustavoaut@gmail.com>
2022-10-08 20:03:00 -0300

    Teste de Commit

--- main.py
+++ main.py
@@ -130,6 +130,7 @@ if __name__ == '__main__':
 
     app = JanelaPrincipal()
     app.master.title('Iniciando REM - Redes de Petri Estocásticas Markovianas')
+    print("Mudança pra teste de commit")
     # app.bind("<Key>", app.buttonSave())
     criar_log_Json()
     app.mainloop()
