25bce9ffee60f6d80d23e5e4a9b49637ecfb984a
Luis G D Simão <luisgustavoaut@gmail.com>
2022-10-07 21:26:10 -0300

    Terceiro Commit: Testando janelas com pacote do python Tkinter

--- main.py
+++ main.py
@@ -6,27 +6,33 @@
 
 import tkinter as tk
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 def increase():
     value = int(lbl_value["text"])
     lbl_value["text"] = f"{value + 1}"
+
 
 def decrease():
     value = int(lbl_value["text"])
     lbl_value["text"] = f"{value - 1}"
 
+
+class JanelaPrincipal(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text="Sair", command=self.quit)
+        self.quitButton.grid()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print('Mudando um pouquinho')
-    a = 1
-    window = tk.Tk()
+    print('Iniciando REM - Redes de Petri Estocásticas Markovianas')
     janela_principal = tk.Tk()
-    janela_principal.title("REM - Redes de Petri Estocásticas Markovianas")
 
+    janela_principal.title("REM - Redes de Petri Estocásticas Markovianas")
     janela_principal.rowconfigure(0, minsize=800, weight=1)
     janela_principal.columnconfigure(1, minsize=800, weight=1)
 
@@ -35,21 +41,7 @@ if __name__ == '__main__':
     btn_open = tk.Button(frm_buttons, text="Open")
     btn_save = tk.Button(frm_buttons, text="Save As...")
 
-    window.resizable(width=True, height=True)
-    #Dimensionando janela
-
-    window.rowconfigure(0, minsize=50, weight=1)
-    window.columnconfigure([0, 1, 2], minsize=50, weight=1)
-
-    btn_decrease = tk.Button(master=window, text="-", command=decrease)
-    btn_decrease.grid(row=0, column=0, sticky="nsew")
-
-    lbl_value = tk.Label(master=window, text="0")
-    lbl_value.grid(row=0, column=1)
-
-    btn_increase = tk.Button(master=window, text="+", command=increase)
-    btn_increase.grid(row=0, column=2, sticky="nsew")
-    window.mainloop()
-
+    # Dimensionando janela
+    janela_principal.mainloop()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
